e5921b70fe291b2f858798d73cc2999e0399c73b
knightbane186 <denverlopes93@gmail.com>
2023-08-29 02:25:11 +1000

    profiling

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 export default function Home() {
   return (
@@ -12,7 +15,7 @@ export default function Home() {
       <main className="bg-white px-10 ">
         <section className=" min-h-screen">
 <nav className="py-10 mb-12 flex justify-between">
-  <h1 className="text-xl">developed by</h1>
+  <h1 className="text-xl font-burtons">developed by</h1>
   <ul className="flex items-center">
     <li>
       <BsFillMoonStarsFill className="cursor-pointer text-xl"/>
@@ -20,7 +23,32 @@ export default function Home() {
     <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 " href="#">Resume</a></li>
   </ul>
 </nav>
+<div className="text-center p-10">
+  <h2 className="text-5xl py-2 text-teal-600 font-medium">Denver Lopes</h2>
+  <h3 className="text-2xl py-2">Full-Stack Developer.</h3>
+  <p className="text-md py-5 leading-8 text-gray-800">
+    A self-taught developer w/ pro skills in web & mobile applications development.
+    Check out my projects down below and take it from there!
+  </p>
+</div>
+<div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+<AiFillGithub/>
+<AiFillLinkedin/>
+</div>
+<div className="relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden">
+  {/* now some  comps below are Next.js specific */}
+  <Image src={deved}  layout="fill" objectFit="cover"/>
+</div>
         </section>
+
+<section>
+  <div>
+    <h3 className="text-3xl py-1">My skills package</h3>
+    <p className="text-md py-2 leading-8 text-gray-800">he code snippet you provided seems to be an Axios POST request in a JavaScript environment, likely a React Native app, <span className="text-teal-500">attempting</span>  to register a user by sending data to a local server. Without more context, it's a bit difficult to <span className="text-teal-500">pinpoint</span> what might be wrong, but I'll try to point out a few potential issues</p>
+    <p className="text-md py-2 leading-8 text-gray-800">he code snippet you provided  in a JavaScript environment, likely a React Native app, <span className="text-teal-500">attempting</span>  to more context, it's a bit difficult to <span className="text-teal-500">pinpoint</span> what might be wrong, but I'll try to point out a few potential issues</p>
+  </div>
+</section>
+
       </main>
     </div>
   );
